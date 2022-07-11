b3ad10e7016ee74f40ec7284c64e1c8dbdeabd07
Tristan Lelong <tlelong@google.com>
2022-07-11 22:50:35 +0200

    Remove unneeded endianess include.

--- revocations.c
+++ revocations.c
@@ -5,7 +5,6 @@
  * license that can be found in the LICENSE file or at
  * https://developers.google.com/open-source/licenses/bsd
  */
-#include <endian.h>
 #include <inttypes.h>
 #include <stddef.h>
 #include <stdlib.h>
