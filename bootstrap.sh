e3e9c7e81d239ba6937c5cdd5783471d42a5d98a
Willy Tu <wltu@google.com>
2022-08-24 01:21:15 +0000

    build: Add bootstrap.sh for hiba
    This script is needed for bitbake to build it properly in
    https://gbmc-review.googlesource.com/c/meta-gbmc-staging/+/2001
    
    Signed-off-by: Willy Tu <wltu@google.com>

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+AUTOCONF_FILES="Makefile.in aclocal.m4 ar-lib autom4te.cache compile \
+        config.guess config.h.in config.sub configure depcomp install-sh \
+        ltmain.sh missing *libtool test-driver"
+
+case $1 in
+    clean)
+        test -f Makefile && make maintainer-clean
+        for file in ${AUTOCONF_FILES}; do
+            find . -name "$file" -print0 | xargs -0 -r rm -rf
+        done
+        exit 0
+        ;;
+esac
+
+autoreconf -fvi
+# shellcheck disable=SC2016
+echo 'Run "./configure ${CONFIGURE_FLAGS} && make"'
