642d64ffec5719178644078867bcd4ff88298621
Damien Miller <djm@google.com>
2025-05-09 10:24:31 +1000

    include sys/types.h before log.h
    OpenSSH 10.0 has some declarations in log.h that use the type `u_int`,
    which is usually provided by the platform's sys/types.h.
    
    Including OpenSSH's `defines.h` might be a better fix, because it
    includes replacement types when the system's headers lack stuff, but
    that's a bit more risky to include as it replaces a bunch of stuff.

--- checks.c
+++ checks.c
@@ -10,6 +10,7 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
--- extensions.c
+++ extensions.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
 #include "config.h"
 
